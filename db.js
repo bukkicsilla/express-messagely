66d41061ac5c9372fca3947c1f3a1952b3c8ef60
Csilla Bukki <csillabukki@Csillas-MacBook-Air.local>
2024-03-18 17:27:41 -0700

    user model fixed

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 /** Database connection for messagely. */
-
+//https://stackoverflow.com/questions/9736085/run-a-postgresql-sql-file-using-command-line-arguments
+//psql messagely < data.sql
 
 const { Client } = require("pg");
 const { DB_URI } = require("./config");
@@ -7,6 +8,5 @@ const { DB_URI } = require("./config");
 const client = new Client(DB_URI);
 
 client.connect();
-
 
 module.exports = client;
